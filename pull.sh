c3f6f042622b26bc62ff28666bcf93845060fc64
Tomasz Klim <github@tomaszklim.pl>
2018-05-12 10:46:44 +0200

    use absolute path for ssh keys

--- pull.sh
+++ pull.sh
@@ -7,15 +7,15 @@ key=$3
 if [ "$3" = "" ]; then
 	echo "usage: $0 <repository-url> <directory> <key-name>"
 	exit 1
-elif [ ! -f /etc/local/.ssh/$key ]; then
+elif [ ! -f $key ]; then
 	echo "git key $key not found"
 	exit 1
 fi
 
 if [ -d $dir ]; then
 	cd $dir
-	GIT_SSH=/opt/farm/scripts/git/helper.sh GIT_KEY=/etc/local/.ssh/$key git pull
+	GIT_SSH=/opt/farm/scripts/git/helper.sh GIT_KEY=$key git pull
 else
 	mkdir -p -m 0711 `dirname $dir`
-	GIT_SSH=/opt/farm/scripts/git/helper.sh GIT_KEY=/etc/local/.ssh/$key git clone $repo $dir
+	GIT_SSH=/opt/farm/scripts/git/helper.sh GIT_KEY=$key git clone $repo $dir
 fi
